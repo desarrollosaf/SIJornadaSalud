14e2e13bff757efc888ec75e1636901405f45cf3
MarSGlez <martin.sanchezg@hotmail.com>
2025-12-01 11:53:31 -0600

    cambios conection server

--- backend/dist/database/cuestionariosConnection.js
+++ backend/dist/database/cuestionariosConnection.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelizeCuestionarios = new sequelize_1.Sequelize('jornadaMujeres', 'homestead', 'secret', {
-    host: '192.168.10.10',
+const sequelizeCuestionarios = new sequelize_1.Sequelize('adminplem_jornadamujeres', 'usr_jornadamujeres', '4UYxXzz6wIuAxvfzN0z7', {
+    host: '192.168.36.53',
     dialect: 'mysql',
     define: {
         freezeTableName: true
--- backend/src/database/cuestionariosConnection.ts
+++ backend/src/database/cuestionariosConnection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
-const sequelizeCuestionarios = new Sequelize('jornadaMujeres', 'homestead', 'secret', {
-    host: '192.168.10.10',
+const sequelizeCuestionarios = new Sequelize('adminplem_jornadamujeres', 'usr_jornadamujeres', '4UYxXzz6wIuAxvfzN0z7', {
+    host: '192.168.36.53',
     dialect: 'mysql',
     define: {
         freezeTableName: true 
